9b52cca2a37d04bf89ed0eea0a24f957af55b4c7
Alexander Macias <alexandermacias59@gmail.com>
2025-12-01 23:14:36 +0100

    css alle card

--- script.js
+++ script.js
@@ -9,35 +9,35 @@
 // }
 // fetch("./assets/data.json").then(manageResponse).then(manageResult);
 
-fetch("./assets/data.json")
-.then(resp => resp.json())
-.then(dataCallback)
-.catch(err => console.error(err))
+// fetch("./assets/data.json")
+// .then(resp => resp.json())
+// .then(dataCallback)
+// .catch(err => console.error(err))
 
-const bookContainer = document.getElementById("book-container");
-function dataCallback(data){
-    for (const book of data) {
-        console.log(book.title +"" + book.author);
-        bookContainer.innerHTML += "<p>titolo: "+ book.title + " , autore: "+ book.author + "</p>"
-    }
-}
+// const bookContainer = document.getElementById("book-container");
+// function dataCallback(data){
+//     for (const book of data) {
+//         console.log(book.title +"" + book.author);
+//         bookContainer.innerHTML += "<p>titolo: "+ book.title + " , autore: "+ book.author + "</p>"
+//     }
+// }
 
 
 //pokeapi
-fetch("https://pokeapi.co/api/v2/pokemon")
-.then(resp => resp.json())
-.then(pokemonCallback)
-.catch(err => console.error(err))
+// fetch("https://pokeapi.co/api/v2/pokemon")
+// .then(resp => resp.json())
+// .then(pokemonCallback)
+// .catch(err => console.error(err))
 
-function pokemonCallback(data){
-const pokemons = data.results;
-const pokeContainer = document.getElementById("pokemon-container");
-for (const pokemon of pokemons) {
-    const pokeP = document.createElement("p");
-    pokeP.appendChild(document.createTextNode(pokemon.name));
-    pokeContainer.appendChild(pokeP);
-}
-}
+// function pokemonCallback(data){
+// const pokemons = data.results;
+// const pokeContainer = document.getElementById("pokemon-container");
+// for (const pokemon of pokemons) {
+//     const pokeP = document.createElement("p");
+//     pokeP.appendChild(document.createTextNode(pokemon.name));
+//     pokeContainer.appendChild(pokeP);
+// }
+// }
 
 
 //yugioh api
@@ -47,7 +47,20 @@ fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=100&offset=0")
 .catch(err => console.error(err))
 
 function yuGiOhCallback(data){
+    const yuGiOhCard = data;
+    const cardYuGiOhContainer = document.getElementById("yuGiOh-container");
+    for (const card of yuGiOhCard.data) {
+        const cardP = `<div class="card-yuGiOh">
+                        <div class= "card-name">${card.name}</div> 
+                        <div class="card-type">[${card.type}]</div>
+                        <div class="card-image">
+                        <img src="${card.card_images[0].image_url_cropped}" alt="${card.name}">
+                        </div>
+                        <div class="card-desc">${card.desc}</div>
+                        </div>`;
+        cardYuGiOhContainer.innerHTML += cardP;
+    }
+    }
 
-}
 
 
